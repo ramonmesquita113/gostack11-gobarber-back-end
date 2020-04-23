3b188928ac3c41945d5efcf943cd1297b1958b38
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-23 16:46:21 -0300

    Criando tabela de agendamentos

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 ```docker
 docker run --name gostack_postgress -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
 ```
+
+##Padrão de nomeação no Banco de dados
+- snack_case
+
+##Criação de Migrations:
+
+```yarn
+yarn typeorm migration:create -n CreateAppointments
+```
+
+##Execução de Migrations:
+
+###Executa o método up() da última migration
+```yarn
+yarn typeorm migration:run
+```
+
+###Executa o método down() da última migration
+```yarn
+yarn typeorm migration:revert
+```
+
+###Exibe as migrations executadas
+```yarn
+yarn typeorm migration:show
+```
+
---
+++ src/database/migrations/1587667039274-CreateAppointments.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointments1587667039274
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            // Just Postgres has 'timestamp with time zone', others databases use timestamp
+            type: 'timestamp with time zone',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
