f7b1f2d4ec173aa645662c08eed8d3f4b667483a
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-23 17:48:40 -0300

    Repositório do TypeORM

--- src/database/migrations/1587667039274-CreateAppointments.ts
+++ src/database/migrations/1587667039274-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1587667039274
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
