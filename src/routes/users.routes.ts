96f94d79be27b0b336e8db77744b94e79dc9dba3
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-27 00:06:30 -0300

    Criando classe de erro

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -22,7 +22,7 @@ usersRoutes.post('/', async (request, response) => {
 
     return response.json(user);
   } catch (error) {
-    return response.status(400).json({ error: error.message });
+    return response.status(error.statusCode).json({ error: error.message });
   }
 });
 
@@ -40,7 +40,7 @@ usersRoutes.patch(
       delete user.password;
       return response.json(user);
     } catch (error) {
-      return response.status(400).json({ error: error.message });
+      return response.status(error.statusCode).json({ error: error.message });
     }
   },
 );
