47ca87871e1d32372dbc44a801cd5978f91f197a
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-06-29 22:28:31 -0300

    Testando criação de usuário

--- src/modules/users/repositories/fakes/FakeUserRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserRepository.ts
@@ -1,27 +1,21 @@
-import { getRepository, Repository } from 'typeorm';
+import { uuid } from 'uuidv4';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 class UsersRepository implements IUsersRepository {
-  private ormRepository: Repository<User>;
-
-  constructor() {
-    this.ormRepository = getRepository(User);
-  }
+  users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
-    const findUser = await this.ormRepository.findOne({
-      where: { id },
-    });
+    const findUser = this.users.find(user => user.id === id);
+
     return findUser;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const findUser = await this.ormRepository.findOne({
-      where: { email },
-    });
+    const findUser = this.users.find(user => user.email === email);
+
     return findUser;
   }
 
@@ -30,13 +24,19 @@ class UsersRepository implements IUsersRepository {
     email,
     password,
   }: ICreateUserDTO): Promise<User> {
-    const user = this.ormRepository.create({ name, email, password });
-    await this.ormRepository.save(user);
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, name, email, password);
+
     return user;
   }
 
-  public async save({ name, email, password }: User): Promise<User> {
-    return this.ormRepository.save({ name, email, password });
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
   }
 }
 
