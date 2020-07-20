564b3d0db23c303bca82caa9d4b5e4bd5f3e69eb
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-07-20 20:09:00 -0300

    Template de e-mails

---
+++ src/shared/container/providers/MailTemplateProvider/fakes/FakeMailTemplateProvider.ts
@@ -0,0 +1,9 @@
+import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
+import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
+
+class FakeMailTemplateProvider implements IMailTemplateProvider {
+  public async parse({ template }: IParseMailTemplateDTO): Promise<string> {
+    return template;
+  }
+}
+export default FakeMailTemplateProvider;
