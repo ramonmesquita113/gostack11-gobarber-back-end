af809bbc154cac4fc542b5495a550beebfab1ab1
Ramon Paiva <rpaiva@vertigo.com.br>
2020-08-12 01:48:10 -0300

    Alteração nome service reset password

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import ForgotPasswordController from '@modules/users/infra/http/controllers/ForgotPasswordController';
-import ResetPasswordController from '@modules/users/infra/http/controllers/ResetPassowordController';
+import ResetPasswordController from '@modules/users/infra/http/controllers/ResetPasswordController';
 
 const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
