bbd043f69e315691aa5198f8efe5bfef44c3d038
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-08-16 21:59:06 -0300

    E-mails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'ramonmesquita113@gmail.com',
+      name: 'Ramon Mesquita',
+    },
+  },
+} as IMailConfig;
