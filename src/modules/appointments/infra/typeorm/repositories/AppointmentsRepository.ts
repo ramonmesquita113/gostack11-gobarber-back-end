b8528de913158b1e02ce39a7b76557184b847b7e
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-08-24 15:07:59 -0300

    Clientes dos agendamentos

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -74,6 +74,7 @@ class AppointmentsRepository implements IAppointmentRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
